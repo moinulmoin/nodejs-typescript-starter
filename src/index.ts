8ea8894c7bd06489eea77480b85191ff94658563
Moinul Moin <moinulmoin101@gmail.com>
2024-01-19 15:39:58 +0600

    feat: :sparkles: intial setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
 import express from "express";
 dotenv.config();
 
 const port = process.env.PORT || 3000;
-
-console.log({ port });
 
 const app = express();
 
@@ -13,5 +11,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Node app listening on port ${port}`);
+  console.info(`Node app listening on port ${port}`);
 });
